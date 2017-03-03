7985a555be1432355294364c06c984372c312032
Edi Carlos note <edi_csilva@hotmail.com>
2017-03-03 11:41:35 -0300

    intents explicita

--- app/src/main/java/br/com/k19/android/cap04/SecondActivity.java
+++ app/src/main/java/br/com/k19/android/cap04/SecondActivity.java
@@ -1,7 +1,11 @@
 package br.com.k19.android.cap04;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Edi Carlos on 03/03/2017.
@@ -13,6 +17,24 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
-    }
 
+        final EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        final EditText ageEditText = (EditText) findViewById(R.id.age_edit_text);
+        Button button = (Button) findViewById(R.id.next_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getEditableText().toString();
+                String age = ageEditText.getEditableText().toString();
+
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("age", age);
+                startActivity(intent);
+            }
+        });
+    }
 }
+
