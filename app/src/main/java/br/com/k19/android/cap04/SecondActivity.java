33b51e594c4667e822446062c97ee6a8d416f718
Edi Carlos note <edi_csilva@hotmail.com>
2017-03-03 08:46:04 -0300

    adicionado o README

---
+++ app/src/main/java/br/com/k19/android/cap04/SecondActivity.java
@@ -0,0 +1,18 @@
+package br.com.k19.android.cap04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Edi Carlos on 03/03/2017.
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+}
